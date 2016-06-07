cf130a3575f1b8b6f3a11bce35ad5ef992168e21
Kurt Werle <kwerle@solarcity.com>
2016-06-06 20:20:31 -0700

    Refactor

--- clown.rb
+++ clown.rb
@@ -3,30 +3,12 @@ require_relative ('clown_list.rb')
 class Clown
   attr_reader :pos, :color, :correct
 
-  def initialize(opts)
-    @pos = opts[:position]
-    @correct = true
-    @clowns_line = opts[:clowns_line]
-    @color = opts[:color]
-
-
-    @known_reds = 0
-    @known_blues = 0
-    count_hats_ahead
+  def initialize(line)
+    @clowns_line = line
   end
 
   def say_answer
     answer = figure_what_answer_to_say
-    @correct = @color == answer
-    {color_spoken: answer, correct: @correct, position: @pos}
-  end
-
-  def hear_answer_result(heard_answer)
-    #This is also where algorithm code would go!
-
-    @pos_last_heard_answer = heard_answer[:position]
-    @last_speaker_correct = heard_answer[:correct]
-    @color_last_speaker_said = heard_answer[:color_spoken]
   end
 
   private
@@ -35,20 +17,23 @@ class Clown
     #This is where you put your algorithm!!
     #Make an algorithm that will save the maximum amount of people for sure
 
-    @known_reds > @known_blues ? :red : :blue
+    known_red > known_blue ? :red : :blue
   end
 
-  def count_hats_ahead
-    i = pos + 1
-    while i < @clowns_line.length
-      seen_color = clowns_line[i].color
-      if seen_color == :red
-        @known_reds += 1
-      else
-        @known_blues += 1
-      end
-      i += 1
-    end
+  def known_red
+    @clowns_line.future_colors(self).count{|color| color == :red}
+  end
+
+  def known_blue
+    @clowns_line.future_colors(self).count{|color| color == :blue}
+  end
+
+  def heard_red
+    @clowns_line.previous_answers(self).count{|color| color == :red}
+  end
+
+  def heard_blue
+    @clowns_line.previous_answers(self).count{|color| color == :blue}
   end
 
 end
--- clown_list.rb
+++ clown_list.rb
@@ -6,6 +6,7 @@ class ClownList
   def initialize
     @rand_gen = Random.new
     @clowns = []
+    @clowns_hash_array = []
     @limit_reds =  @rand_gen.rand(100)# < 50 ? :red : :blue
     @limit_blues = 100 - @limit_reds
 
@@ -15,29 +16,40 @@ class ClownList
     put_hats_on_clowns
   end
 
+  def length
+    @clowns.length
+  end
 
+  def num_color(color)
+    @clowns_hash_array.select{ |clown_hash| clown_hash[:color_of_hat] == color }.count
+  end
+
+  def correct_clowns
+    @clowns_hash_array.select{ |clown_hash| clown_hash[:color_of_hat] == clown_hash[:guessed_color_of_hat] }.count
+  end
+
+  def previous_answers(clown)
+    return @clowns_hash_array[0..(clown_index(clown) - 1)].map{ |clown_hash| clown_hash[:guessed_color_of_hat] }
+  end
+
+  def future_colors(clown)
+    return @clowns_hash_array[((clown_index(clown)) + 1)..-1].map{ |clown_hash| clown_hash[:color_of_hat] }
+  end
+
+  def clown_guesses(clown, color)
+    @clowns_hash_array[clown_index(clown)][:guessed_color_of_hat] = color
+  end
+
+  private
 
   def [](pos)
     @clowns[pos]
   end
 
-  def length
-    @clowns.length
+  def clown_index(clown)
+    @clowns.index(clown)
   end
 
-  def dead_clowns
-    count = 0
-    @clowns.each do |clown|
-      count += 1 if !clown.correct
-    end
-    count
-  end
-
-  def correct_clowns
-    @clowns.length - dead_clowns
-  end
-
-  private
   def can_make_this_hat(color_of_hat)
     if color_of_hat == :red
       return @num_reds < @limit_reds
@@ -48,20 +60,21 @@ class ClownList
 
   def put_hats_on_clowns
 
-    idx = 0
-    while @clowns.length < 100 do
+    100.times do
       color_of_hat = @rand_gen.rand(100) < 50 ? :red : :blue
 
-      if can_make_this_hat(color_of_hat)
-        @clowns.push(
-          Clown.new({position: idx, clowns_line: self, color: color_of_hat})
-        )
-
-        #increment position for next clown
-        idx += 1
-        #increment the number of the type of hat we placed
-        color_of_hat == :red ? (@num_reds += 1) : (@num_blues += 1)
+      if !can_make_this_hat(color_of_hat)
+        if color_of_hat == :red
+          color_of_hat = :blue
+        else
+          color_of_hat = :red
+        end
       end
+      @clowns << Clown.new(self)
+      @clowns_hash_array << {color_of_hat: color_of_hat}
+
+      #increment the number of the type of hat we placed
+      color_of_hat == :red ? (@num_reds += 1) : (@num_blues += 1)
 
     end
   end
